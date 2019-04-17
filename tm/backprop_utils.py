223beca26b571b42c01751904917b31fd704d82e
robertdanielh <robert.herscovici@geminicad.com>
2019-04-17 16:50:10 +0300

    compute actual loss and div by length

--- tm/backprop_utils.py
+++ tm/backprop_utils.py
@@ -11,14 +11,14 @@ def backward(net: TM, conf_eval_losses, final_classifier_losses, q_m_losses, a_m
     _set_requires_grad(net, False)
     _set_requires_grad(net.conf_eval, True)
 
-    total_conf_eval_loss = sum(loss.mean() for loss in conf_eval_losses)
+    total_conf_eval_loss = sum(loss.mean() for loss in conf_eval_losses)/len(conf_eval_losses)
     total_conf_eval_loss.backward(retain_graph=True)
 
     # Final classifier: should only use the loss at the last layer for each sample
     _set_requires_grad(net, False)
     _set_requires_grad(net.f_cls, True)
 
-    total_final_classifier_loss = sum(fc_loss[-1] for fc_loss in final_classifier_losses)
+    total_final_classifier_loss = sum(fc_loss[-1] for fc_loss in final_classifier_losses)/len(final_classifier_losses)
 
     total_final_classifier_loss.backward(retain_graph=True)
 
@@ -26,8 +26,10 @@ def backward(net: TM, conf_eval_losses, final_classifier_losses, q_m_losses, a_m
     # Answer machine:
     _set_requires_grad(net, True)
 
-    total_q_m_loss = sum(loss.mean() for loss in q_m_losses)
-    total_a_m_loss = sum(loss.mean() for loss in a_m_losses)
+    total_q_m_loss = sum(loss.mean() for loss in q_m_losses)/len(q_m_losses)
+    total_a_m_loss = sum(loss.mean() for loss in a_m_losses)/len(a_m_losses)
 
     total_q_m_loss.backward(retain_graph=True)
     total_a_m_loss.backward(retain_graph=True)
+
+    return total_conf_eval_loss.item(), total_final_classifier_loss.item(), total_a_m_loss.item(), total_q_m_loss.item()
